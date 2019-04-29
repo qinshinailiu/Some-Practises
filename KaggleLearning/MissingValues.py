7eaa085fbe6a7b9bf7061945992fcd2beab2835a
qinshinailiu <276597325@qq.com>
2019-04-29 19:40:33 +0800

    缺失值处理

---
+++ KaggleLearning/MissingValues.py
@@ -0,0 +1,70 @@
+import pandas as pd 
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+'''
+缺失值处理的三种方法
+1.直接删除column ------谁想出来的？
+2.Imputation：用列上的mean去填补缺失值 
+3.填补之后，再新加一个列，用true和false表示是否填补（缺失
+
+'''
+data = pd.read_csv('melb_data.csv')
+y = data.Price
+
+#只选取数字特征
+melb_predictors = data.drop(['Price'],axis=1)
+X = melb_predictors.select_dtypes(exclude=['object'])
+X_train,X_valid,y_train,y_valid = train_test_split(X,y,train_size = 0.8,test_size = 0.2,random_state=0)
+
+def score_dataset(X_train,X_valid,y_train,y_valid):
+	model = RandomForestRegressor(n_estimators = 10)
+	model.fit(X_train,y_train)
+	preds = model.predict(X_valid)
+	return mean_absolute_error(y_valid,preds)
+
+#isnull 生成series 所以再加个any
+# any是有t则t 全f则f
+cols_with_missing = [col for col in X_train.columns if X_train[col].isnull().any()]
+
+reduced_X_train = X_train.drop(cols_with_missing,axis = 1)
+reduced_X_valid = X_valid.drop(cols_with_missing,axis = 1)
+
+print('MAE from Approach 1:(Drop columns with missing values):')
+print(score_dataset(reduced_X_train,reduced_X_valid,y_train,y_valid))
+
+from sklearn.impute import SimpleImputer
+
+my_imputer = SimpleImputer()
+imputed_X_train = pd.DataFrame(my_imputer.fit_transform(X_train))
+imputed_X_valid = pd.DataFrame(my_imputer.transform(X_valid))
+
+#imputation没有columns
+imputed_X_train.columns = X_train.columns
+imputed_X_valid.columns = X_valid.columns
+
+print('MAE from Approach 2 (Imputation):')
+print(score_dataset(imputed_X_train,imputed_X_valid,y_train,y_valid))
+
+X_train_plus = X_train.copy()
+X_valid_plus = X_valid.copy()
+
+for col in cols_with_missing:
+	X_train_plus[col+'_was_missing'] = X_train_plus[col].isnull()
+	X_valid_plus[col+'_was_missing'] = X_valid_plus[col].isnull()
+
+my_imputer = SimpleImputer()
+imputed_X_train_plus = pd.DataFrame(my_imputer.fit_transform(X_train_plus))
+imputed_X_valid_plus = pd.DataFrame(my_imputer.fit_transform(X_valid_plus))
+
+imputed_X_train_plus.columns = X_train_plus.columns
+imputed_X_valid_plus.columns = X_valid_plus.columns
+
+print('MAE from Approach 3 (An Extension to Imputation):')
+print(score_dataset(imputed_X_train_plus,imputed_X_valid_plus,y_train,y_valid))
+
+print(X_train.shape)
+
+missing_val_count_by_column = (X_train.isnull().sum())
+#print(missing_val_count_by_column)
+print(missing_val_count_by_column[missing_val_count_by_column > 0])
